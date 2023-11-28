e8c70dacb67e1fa9cd9c4d4e5273d4aecf751a4a
pratham_19 <prathamsinghal2003@gmail.com>
2023-11-28 22:33:54 +0530

    initial commit

---
+++ app/_utils/Provider/index.tsx
@@ -0,0 +1,11 @@
+// ("");
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // Add the Country Identity Provider at the root of your app
+    <AnonAadhaarProvider>{children}</AnonAadhaarProvider>
+  );
+};
+
+export default Providers;
